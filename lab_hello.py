bf15a5a502bf0963f17390ac49bbf65ac504f175
Zhdannikov Arslan <zdannikovarslan@gmail.com>
2024-02-06 11:55:38 +0300

    lab

---
+++ lab_hello.py
@@ -0,0 +1,14 @@
+print("Привет")
+
+name = "Маша"
+print ("Привет "+name+"!")
+print ("Привет ",name,"!")
+print (f"Привет, {name}!")
+
+def print_sum():
+    a = 1
+    b = 2
+    print(a + b)
+
+print_sum()
+
